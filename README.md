2a31ca2b8048e61f5934cd02f13c1d447743b6f4
Greg McKeever <greg@librato.com>
2017-05-31 15:56:52 -0700

    Flatten JSON payload for QuickSight analysis

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Chalice Projets
 
 My chalice projects for AWS Lambda
+
+# Using Chalice
+
+Instructions on using Chalice can be found in the official [Chalice repo](https://github.com/awslabs/chalice)
--- webhook-to-s3/app.py
+++ webhook-to-s3/app.py
@@ -4,6 +4,7 @@ from urllib.parse import parse_qs
 import json
 import time
 import boto3
+from flatten_json import flatten
 #from importlib import reload
 #from hashlib import sha256
 
@@ -23,13 +24,21 @@ def chargify():
     #if not hmac.compare_digest(signature, hmac.new(CHARGIFY_SHARED_KEY, request.get_data(), sha256).hexdigest()): abort(404)
 
     request = app.current_request
+
     try:
         webhook_data = str(request.raw_body)
     except KeyError:
         raise BadRequestError()
 
+    ## used for 'chalice local' testing
+    #request = "insert dummy chargify payload"
+    #data=parse_chargify_webhook(request)
     data=parse_chargify_webhook(webhook_data)
+
     json_str=json.dumps(data).replace('[', '').replace(']', '')
+    json_dict=flatten(eval(json_str))
+    json_str=str(json_dict)
+
     date = parse_time(str(datetime.now()))
     filename = data['id'][0] + '.json'
     file_path = 'billing/year={year}/month={month}/date={day}/'.format(**date) + filename
@@ -40,6 +49,14 @@ def chargify():
         Body=json_str,
         ACL='private'
     )
+
+    ## used for 'chalice local' testing
+    '''
+    file_path = 'data.json'
+
+    with open(file_path, 'w+') as fp:
+        json.dump(json_dict, fp)
+    '''
 
     return {
         'url': 'https://s3.amazonaws.com/{}/{}'.format(s3_bucket, file_path)
@@ -54,11 +71,10 @@ def parse_time(timestamp):
             pass
     raise ValueError('no valid date format found')
 
+# Converts Chargify webhook parameters to a python dictionary of nested dictionaries
 def parse_chargify_webhook(post_data):
-    '''
-    Converts Chargify webhook parameters to a python dictionary of nested dictionaries
-    '''
     post_data = parse_qs(post_data)
+
     result = {}
     for k, v in iter(post_data.items()):
         keys = [x.strip(']') for x in k.split('[')]
